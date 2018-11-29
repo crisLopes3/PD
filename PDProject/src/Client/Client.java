f67bc30bbf1f2a0cb2b5d62908980053cb14d35a
crisLopes3 <cristianocnlopes@gmail.com>
2018-11-29 16:32:12 +0000

    v2

--- PDProject/src/Client/Client.java
+++ PDProject/src/Client/Client.java
@@ -3,33 +3,21 @@ package Client;
 import Server.Registo;
 import java.net.*;
 import java.io.*;
-import java.util.*;
 
 public class Client {
 
-     public static final int MAX_SIZE = 256;
-    public static final String TIME_REQUEST = "TIME";
     public static final int TIMEOUT = 10; //segundos
 
     public static void main(String[] args) throws IOException
     {
         
-        InetAddress serverAddr = null;
-        int serverPort = -1;
         Socket socket = null;
         ObjectOutputStream out = null;
         ObjectInputStream in = null;
         String response;
-        
-//        if(args.length != 2){
-//            System.out.println("Sintaxe: java TcpTimeClient serverAddress serverUdpPort");
-//            return;
-//        }
 
         try{
-
-//            serverAddr = InetAddress.getByName("localhost");
-//            serverPort = Integer.parseInt(args[1]);   
+ 
             socket = new Socket("localhost", 6001);
             socket.setSoTimeout(TIMEOUT*1000);
             
@@ -39,8 +27,7 @@ public class Client {
             out.writeObject(new Registo("cris", "pass2"));
             out.flush();
             
-            //A resposta deve terminar com uma mundança de linha.
-            //Os caracteres de mudança de linha não são copiados para "response"
+
             response = (String)in.readObject();
           
             System.out.println(response);
--- PDProject/src/Server/Server.java
+++ PDProject/src/Server/Server.java
@@ -1,6 +1,5 @@
 package Server;
 
-import java.util.*;
 import java.net.*;
 import java.io.*;
 
@@ -8,61 +7,52 @@ public class Server {
 
     public static final int MAX_SIZE = 256;
     public static final String TIME_REQUEST = "TIME";
-    
-    private ServerSocket socket;
-    private boolean debug;
 
-    public Server(String args[], boolean debug)
-    {
-        int listeningPort;
+    private ServerSocket socket;
+
+    public Server(String args[], boolean debug) {
 
         socket = null;
-        this.debug = debug;
-        
-        try{
-            
-//            listeningPort = Integer.parseInt(args[0]);
+        try {
             socket = new ServerSocket(6001);
-            
-        }catch(NumberFormatException e){
-            System.out.println("O porto de escuta deve ser um inteiro positivo:\n\t"+e);
-        }catch(IOException e){
-            System.out.println("Ocorreu um erro na criação do socket de escuta:\n\t"+e);
+        } catch (NumberFormatException e) {
+            System.out.println("O porto de escuta deve ser um inteiro positivo:\n\t" + e);
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro na criação do socket de escuta:\n\t" + e);
             socket = null;
-        }             
+        }
     }
-    
-    public final void processRequests()
-    {
+
+    public final void processRequests() {
         Socket toClientSocket;
         ObjectInputStream in;
         ObjectOutputStream out;
         Registo request;
-        String resposta="";    
-        
-        if(socket == null){
+        String resposta = "";
+
+        if (socket == null) {
             return;
         }
-        
+
         System.out.println("TCP Serialized Time Server iniciado no porto " + socket.getLocalPort() + " ...");
-        
-        while(true){     
-            
-            try{
+
+        while (true) {
+
+            try {
                 toClientSocket = socket.accept();
-            }catch(IOException e){
-                System.out.println("Erro enquanto aguarda por um pedido de ligação:\n\t"+e);
+            } catch (IOException e) {
+                System.out.println("Erro enquanto aguarda por um pedido de ligação:\n\t" + e);
                 return;
             }
 
-            try{
-                
-                out = new ObjectOutputStream(toClientSocket.getOutputStream());            
+            try {
+
+                out = new ObjectOutputStream(toClientSocket.getOutputStream());
                 in = new ObjectInputStream(toClientSocket.getInputStream());
 
-                request = (Registo)(in.readObject());
+                request = (Registo) (in.readObject());
 
-                if(request == null){ //EOF
+                if (request == null) { //EOF
                     toClientSocket.close();
                     continue; //to next client request
                 }
@@ -74,35 +64,27 @@ public class Server {
                 out.writeObject(resposta);
                 out.flush();
 
-            }catch(IOException e){
-                 System.out.println("Erro na comunicação como o cliente " + 
-                    toClientSocket.getInetAddress().getHostAddress() + ":" + 
-                        toClientSocket.getPort()+"\n\t" + e);
-            }catch (ClassNotFoundException e){
-                System.out.println("Pedido recebido de tipo inesperado:\n\t"+e);
-            }finally{
-                try{
-                    if(toClientSocket != null){
+            } catch (IOException e) {
+                System.out.println("Erro na comunicação como o cliente "
+                        + toClientSocket.getInetAddress().getHostAddress() + ":"
+                        + toClientSocket.getPort() + "\n\t" + e);
+            } catch (ClassNotFoundException e) {
+                System.out.println("Pedido recebido de tipo inesperado:\n\t" + e);
+            } finally {
+                try {
+                    if (toClientSocket != null) {
                         toClientSocket.close();
                     }
-                }catch(IOException e){}
+                } catch (IOException e) {
+                }
             }
         } //while
     }
-       
-    public static void main(String[] args) 
-    {
-        
+
+    public static void main(String[] args) {
         Server timeServer;
-//        
-//        if(args.length != 1){
-//            System.out.println("Sintaxe: java TcpTimeServer listeningPort");
-//            return;
-//        }
-        
+
         timeServer = new Server(args, true);
         timeServer.processRequests();
-       
     }
 }
-
