39eea3e81bc473b2720fd17aa4941ffa6864d78f
socio005 <45179042+socio005@users.noreply.github.com>
2018-11-29 14:08:37 +0000

    Comit Pedro

---
+++ PDProject/src/Server/Registo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Server;
+
+/**
+ *
+ * @author Pedro
+ */
+public class Registo {
+    private String userName;
+    private String passWord;
+
+    public Registo(String userName, String passWord) {
+        this.userName = userName;
+        this.passWord = passWord;
+    }
+    
+    
+}
