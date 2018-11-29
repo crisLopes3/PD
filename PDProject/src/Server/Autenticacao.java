8f5740582e8465e86a36aafdf89b36b23ff16368
socio005 <45179042+socio005@users.noreply.github.com>
2018-11-29 17:30:54 +0000

    PedroUpdate

---
+++ PDProject/src/Server/Autenticacao.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Server;
+
+import java.net.InetAddress;
+
+
+
+/**
+ *
+ * @author Pedro
+ */
+public class Autenticacao {
+    private Registo registo;
+    private InetAddress ipCliente;
+
+    public Autenticacao(Registo registo, InetAddress ipCliente) {
+        this.registo = registo;
+        this.ipCliente = ipCliente;
+    }
+
+    public Registo getRegisto() {
+        return registo;
+    }
+
+    public InetAddress getIpCliente() {
+        return ipCliente;
+    }
+    
+    
+}
--- PDProject/src/Server/Registo.java
+++ PDProject/src/Server/Registo.java
@@ -1,9 +1,9 @@
-
 package Server;
 
 import java.io.Serializable;
 
-public class Registo implements Serializable{
+public class Registo implements Serializable {
+
     private String userName;
     private String passWord;
 
@@ -11,8 +11,22 @@ public class Registo implements Serializable{
         this.userName = userName;
         this.passWord = passWord;
     }
-    
-    
-    
-    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj != null) {
+            return false;
+        }
+        if (!(obj instanceof Registo)) {
+            return false;
+        }
+
+        Registo aux = (Registo) obj;
+        if (aux.passWord.equals(this.passWord) && aux.userName.equals(this.userName)) {
+            return true;
+        }
+
+        return false;
+    }
+
 }
