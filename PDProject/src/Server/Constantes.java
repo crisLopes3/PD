5aec7d84207e31404c822dd0551f0b7c73560ed3
socio005 <45179042+socio005@users.noreply.github.com>
2018-12-05 12:47:41 +0000

    ConectarBaseDados

---
+++ PDProject/src/Server/Constantes.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Server;
+
+/**
+ *
+ * @author Pedro
+ */
+public interface Constantes {
+    public static final String DRIVER="com.mysql.jdbc.Driver";
+    public  static final String USERNAME="root";
+    public  static final String PASSWORD="";
+    public  static final String  CONN_STRING="jdbc:mysql://localhost:3306/pdbasedados?useTimezone=true&serverTimezone=UTC";
+
+}
--- PDProject/src/Server/Server.java
+++ PDProject/src/Server/Server.java
@@ -2,10 +2,13 @@ package Server;
 
 import java.net.*;
 import java.io.*;
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Server {
+public class Server implements Constantes{
 
     public ServerSocket socketAutenticacao;
     public ServerSocket socketRegista;
@@ -38,28 +41,38 @@ public class Server {
         return t1!=null && t2!=null;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         Server timeServer;
 
-        timeServer = new Server(args, true);
-        timeServer.lancaThreads();
+        Connection conn=null;
         
-        try {
-            System.out.println("Comando:");
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-           
-            while (true) {
-
-                String cast = in.readLine();
-
-                if (cast.equals("exit")) {
-                    return;
-                }
-            }
-        } catch (Exception e) {
-
+        try{
+            System.out.println("Connected");
+            conn=DriverManager.getConnection(CONN_STRING,USERNAME,PASSWORD);
+            System.out.println("Connected");
+        }catch(SQLException e){
+            System.err.print(e);
         }
         
+//        timeServer = new Server(args, true);
+//        timeServer.lancaThreads();
+        
+//        try {
+//            System.out.println("Comando:");
+//            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//           
+//            while (true) {
+//
+//                String cast = in.readLine();
+//
+//                if (cast.equals("exit")) {
+//                    return;
+//                }
+//            }
+//        } catch (Exception e) {
+//
+//        }
+//        
     }
 
 }
